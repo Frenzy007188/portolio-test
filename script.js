8ef0c5226227221ce7bb6f09f730ab1ab1fe7436
Bellothompson david <bellothompsondavid@gmail.com>
2025-02-26 19:58:52 +0100

    my commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('contact-form').addEventListener('submit',
+    function(event){
+        event.preventDefault();
+
+        const name = 
+        document.getElementById('name').value;
+
+        const email = 
+        document.getElementById('email').value;
+
+        const message = document.getElementById('message').value;
+
+        if(!name || !email || !message) {
+         alert("pls fill the informations")
+        return;
+    }
+        console.log("formsubmitted:",{name , email ,message});
+
+        document.getElementById('contact-form').reset();
+        alert("Thanks for visting Frenzy Store")
+
+
+
+    });
